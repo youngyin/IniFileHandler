f87b4f55ceacf4cdb42230eb61bf41c10c3e0320
youngyin <57608725+youngyin@users.noreply.github.com>
2024-09-09 04:28:34 +0900

    [feat] 스레드 custom #3

---
+++ src/scheduler/CustomThread.cpp
@@ -0,0 +1,67 @@
+﻿//---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include "CustomThread.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+
+__fastcall TCustomThread::TCustomThread(
+    int nInterval, std::function<void()> fnCallback, TDateTime dtStartTime) :
+    TThread(true),
+	m_nInterval(nInterval), m_fnCallback(fnCallback), m_bIsRunning(false)
+{
+	//FreeOnTerminate = true; // 자동으로 메모리 해제
+}
+
+void __fastcall TCustomThread::Execute()
+{
+	m_bIsRunning = true;
+	m_dtStartTime = Now();
+
+    // 시작 시간 설정
+    while (!Terminated) {
+		TDateTime dtCurrentTime = Now();
+        if (dtCurrentTime >= m_dtStartTime) {
+            // 콜백 실행
+            if (m_fnCallback) {
+                m_fnCallback();
+            }
+            // 다음 실행 시간 설정
+			m_dtStartTime += m_nInterval / 86400.0; // 초를 일수로 변환
+        }
+        Sleep(1000); // 1초 간격으로 현재 시간을 확인
+    }
+
+    m_bIsRunning = false;
+}
+
+void TCustomThread::StartThread()
+{
+    if (!m_bIsRunning) {
+        Start(); // TThread의 Start() 메서드 호출
+    }
+}
+
+void TCustomThread::StopThread()
+{
+    if (m_bIsRunning) {
+		Terminate(); // 스레드 종료 요청
+        WaitFor(); // 스레드가 종료될 때까지 대기
+    }
+}
+
+bool TCustomThread::IsRunning()
+{
+    return m_bIsRunning;
+}
+
+void TCustomThread::SetInterval(int nInterval)
+{
+    if (m_bIsRunning) {
+        throw std::runtime_error(
+            "Thread is running. Please stop it before changing the interval.");
+    }
+    m_nInterval = nInterval;
+}
+
---
+++ src/scheduler/CustomThread.h
@@ -0,0 +1,31 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef CustomThreadH
+#define CustomThreadH
+//---------------------------------------------------------------------------
+
+#include <System.Classes.hpp>
+#include <System.SysUtils.hpp>
+#include <functional>
+#include <atomic>
+
+class TCustomThread : public TThread
+{
+  private:
+    std::atomic<bool> m_bIsRunning; // 스레드 실행 상태
+    int m_nInterval; // Interval in seconds
+    TDateTime m_dtStartTime; // 시작 시간
+    std::function<void()> m_fnCallback; // 콜백 함수
+  protected:
+    void __fastcall Execute() override;
+  public:
+    __fastcall TCustomThread(int nInterval, std::function<void()> fnCallback,
+        TDateTime dtStartTime = 0);
+    void StartThread();
+    void StopThread();
+    bool IsRunning();
+    void SetInterval(int nInterval);
+};
+
+#endif
+
